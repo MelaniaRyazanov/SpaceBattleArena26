fbdb07131040275449a2a4fc417e3c7176c47165
MelaniaRyazanov <melania.ryazanov@gmail.com>
2026-05-21 09:34:14 -0700

    space ship

---
+++ ExampleShip.java
@@ -0,0 +1,23 @@
+import java.awt.Color;
+
+import ihs.apcs.spacebattle.*;
+import ihs.apcs.spacebattle.commands.*;
+
+public class ExampleShip extends BasicSpaceship {
+    public static void main(String[] args)
+    {
+        TextClient.run("10.56.98.121", new ExampleShip());
+    }
+
+    @Override
+    public RegistrationData registerShip(int numImages, int worldWidth, int worldHeight)
+    {
+        return new RegistrationData("Melania", new Color(55, 255, 255), 0);
+    }
+
+    @Override
+    public ShipCommand getNextCommand(BasicEnvironment env)
+    {
+        return new IdleCommand(0.1);
+    }
+}
